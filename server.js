1ea668184c7a16d61f4fa97ef1163300f12aae5d
jasondawson <jason.c.dawson@gmail.com>
2015-03-31 10:32:08 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var Express = require('express');
+
+var port = process.env.EXPRESS_PORT || 8080;
+
+var app = Express();
+app.use(Express.static(__dirname + '/app'));
+
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
