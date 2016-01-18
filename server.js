1f0599e5aab533908d2d4c03ed6f8b5994b28c1f
jasondawson <jason.c.dawson@gmail.com>
2016-01-18 00:16:55 -0700

    added game section and text adventure

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-var Express = require('express');
+var express = require('express');
 var port = process.env.EXPRESS_PORT || 8080;
+var compress = require('compression');
 
+var app = express();
+app.use(compress());
+var oneDay = 86400000;
 
-var app = Express();
-app.use(Express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/app', { maxAge: oneDay }));
+
+app.get('/textAdventure', function (req, res) {
+    
+    res.redirect('/games/TextAdventure/TextAdventure.html');
+})
 
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
